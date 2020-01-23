d211fc4aa8cc6da471b2f0599f442e907089d73e
Mohammed Al-Maqbli <Mohammed.Almaqbli@gmail.com>
2020-01-23 15:46:51 +0300

    Updates

---
+++ precache-manifest.3b46c6b6cdfe2f4b68c8f6462ebdbc38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f230a74275653819c4ff2a8466d73fe",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "ab5de72f5fee38d0bc36",
+    "url": "/ReactPortfolio/static/js/2.4a7ee6df.chunk.js"
+  },
+  {
+    "revision": "baf6806a8d8a8b1a4636d3213c0271fb",
+    "url": "/ReactPortfolio/static/js/2.4a7ee6df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa70eaedf91202506d85",
+    "url": "/ReactPortfolio/static/js/main.fb2c4b03.chunk.js"
+  },
+  {
+    "revision": "e6547e0cab95532fe9f0",
+    "url": "/ReactPortfolio/static/js/runtime-main.8004c86c.js"
+  },
+  {
+    "revision": "954b0f36b38e4662d26df52011c5aa12",
+    "url": "/ReactPortfolio/static/media/1.954b0f36.jpg"
+  },
+  {
+    "revision": "f15cefd79cfd0daa744d94821b45af17",
+    "url": "/ReactPortfolio/static/media/2.f15cefd7.jpg"
+  },
+  {
+    "revision": "dd5f23649c57c0e1454b2c9a7336b4a2",
+    "url": "/ReactPortfolio/static/media/4.dd5f2364.jpg"
+  },
+  {
+    "revision": "5212c2e2468ff3c406edc2da2643dd4d",
+    "url": "/ReactPortfolio/static/media/ring.5212c2e2.png"
+  }
+]);
