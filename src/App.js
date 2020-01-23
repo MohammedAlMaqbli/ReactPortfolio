47ce612cc9232ec1e62a834abcc86f655ed1db1e
Mohammed Al-Maqbli <Mohammed.Almaqbli@gmail.com>
2020-01-23 10:56:51 +0300

    adding animations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import "ionicons/dist/ionicons";
-
 import NavBar from "./Components/NavBar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
@@ -8,12 +7,113 @@ import Skills from "./Components/Skills";
 import Work from "./Work";
 import Contact from "./Components/Contact";
 import Footer from "./Footer";
+import gsap from "gsap";
+import { useIntersection } from "react-use";
 
 const App = () => {
+  // NavBar Animation init
+  const navTl = new gsap.timeline({ paused: true });
+  // Website Animation function
+  const animate = () => {
+    const tl = gsap.timeline({ defaults: { duration: 1 } });
+    tl.from(
+      "header",
+      {
+        duration: 2,
+        delay: 0,
+        y: "-110%",
+        ease: "Power4.out"
+      },
+      "-=0.1"
+    )
+      .from(".ringOne", {
+        duration: 4,
+        delay: 0,
+        opacity: 0,
+        y: 180,
+        ease: "Power4.out"
+      })
+      .from(
+        ".ringTwo",
+        4,
+        {
+          duration: 4,
+          delay: 0.9,
+          opacity: 0,
+          y: 180,
+          ease: "Power4.out"
+        },
+        "-=5"
+      )
+      .to(".ringOne", {
+        duration: 4,
+        delay: 0.4,
+        x: 380,
+        ease: "Power4.out"
+      })
+      .to(
+        ".ringTwo",
+        {
+          duration: 4,
+          delay: 0.9,
+          x: 380,
+          ease: "Power4.out"
+        },
+        "-=5"
+      )
+
+      .from(
+        ".animate-in1",
+        3,
+        {
+          opacity: 0,
+          y: 20,
+          ease: "Power4.out"
+        },
+        "-=2.9"
+      )
+      .from(
+        ".animate-in",
+        3,
+        {
+          opacity: 0,
+          y: 20,
+          ease: "Power4.out"
+        },
+        "-=1.9"
+      )
+      .to("main", 1, { display: "block" });
+  };
+  // NavBar Animation function
+  const animateNav = () => {
+    navTl
+      .to(".overlayer", {
+        duration: 1,
+        y: "100%",
+        width: "100%",
+        ease: "Power4.out"
+      })
+      .from(".nav-item", {
+        duration: 1,
+        x: -200,
+        opacity: 0,
+        stagger: 0.3,
+        ease: "Power4.out"
+      });
+    navTl.reverse();
+  };
+  //  NavBar Animation open and close Nav
+  const handelMenuClick = e => {
+    navTl.reversed() ? navTl.play() : navTl.reverse();
+  };
+  useEffect(() => {
+    animate();
+    animateNav();
+  }, []);
   return (
     <Fragment>
       <header>
-        <NavBar />
+        <NavBar onClick={handelMenuClick} />
         <Hero />
       </header>
       <main class="animate-in1">
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,37 +1,37 @@
 import React, { Fragment } from "react";
 
-const NavBar = () => (
+const NavBar = ({ onClick }) => (
   <Fragment>
     <div className="overlayer"></div>
     <nav className="main-nav animate-in1">
       <div className="logo animate-in">moe.abo</div>
-      <div className="menu-btn  animate-in">
+      <div className="menu-btn  animate-in" onClick={onClick}>
         <ion-icon name="options"></ion-icon>
       </div>
       <ul className="menu">
         <li className="nav-item"></li>
         <li className="nav-item">
-          <a href="#home" className="nav-link">
+          <a href="#home" className="nav-link" onClick={onClick}>
             home
           </a>
         </li>
         <li className="nav-item">
-          <a href="#about" className="nav-link">
+          <a href="#about" className="nav-link" onClick={onClick}>
             about
           </a>
         </li>
         <li className="nav-item">
-          <a href="#skills" className="nav-link">
+          <a href="#skills" className="nav-link" onClick={onClick}>
             skills
           </a>
         </li>
         <li className="nav-item">
-          <a href="#work" className="nav-link">
+          <a href="#work" className="nav-link" onClick={onClick}>
             work
           </a>
         </li>
         <li className="nav-item">
-          <a href="#contact" className="nav-link">
+          <a href="#contact" className="nav-link" onClick={onClick}>
             contact
           </a>
         </li>
